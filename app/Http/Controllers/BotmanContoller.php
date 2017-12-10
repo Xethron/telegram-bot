b7691aeef7cd2c2e3eb7ce5b81f11068544b2d24
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2017-12-10 21:50:44 +0200

    Setup Botman to respond to Telegram messages with some basic usage

--- app/Http/Controllers/BotmanContoller.php
+++ app/Http/Controllers/BotmanContoller.php
@@ -2,10 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Drivers\DriverManager;
+use BotMan\Drivers\Telegram\TelegramDriver;
+use Illuminate\Http\Request;
+
 class BotmanContoller extends Controller
 {
     public function handle()
     {
+        DriverManager::loadDriver(TelegramDriver::class);
 
+        /** @var BotMan $botman */
+        $botman = app('botman');
+
+        $botman->hears('/getBTCEquivalent {value} {currency}', function (BotMan $bot, $value, $currency) {
+            $json = json_decode(file_get_contents('https://api.coindesk.com/v1/bpi/currentprice/'.$currency.'.json'));
+
+            $exchangeRate = $json->bpi->{$currency}->rate_float;
+
+            $valueInBtc = $value/$exchangeRate;
+
+            $bot->reply("$value $currency is ".number_format($valueInBtc, 6)." BTC (".number_format($exchangeRate, 4)." $currency - 1 BTC)");
+        });
+
+        $botman->hears('/getUserID', function (BotMan $bot) {
+            $bot->reply(2);
+        });
+
+        $botman->listen();
     }
 }
