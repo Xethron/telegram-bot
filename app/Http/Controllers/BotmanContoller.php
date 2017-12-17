18377d3295be2a47276b288d387694e58ca91053
Bernhard Breytenbach <bernhard@coffeecode.co.za>
2017-12-17 23:40:24 +0200

    Add some helper commands to make the bot more user friendly

--- app/Http/Controllers/BotmanContoller.php
+++ app/Http/Controllers/BotmanContoller.php
@@ -39,12 +39,28 @@ class BotmanContoller extends Controller
         $botman =  BotManFactory::create($config, new LaravelCache(), app('request'),
             new FileStorage(storage_path('botman')));
 
+        $botman->hears('/start', function (BotMan $bot) {
+            $bot->reply($this->getAvailableCommands());
+        });
+
+        $botman->hears('/help', function (BotMan $bot) {
+            $bot->reply($this->getAvailableCommands());
+        });
+
+        $botman->fallback(function($bot) {
+            $bot->reply("Sorry, I did not understand these commands. Here is a list of commands I understand:\n\n".$this->getAvailableCommands());
+        });
+
         $botman->hears('/getBTCEquivalent {message}', function (BotMan $bot, $message) {
             $messageParts = explode(' ', $message);
             if (count($messageParts) === 1) {
                 $messageParts[]  = BotConfig::get('currency');
             }
             $bot->reply($this->getBitcoinEquivalent($messageParts[0], $messageParts[1]));
+        });
+
+        $botman->hears('/getBTCEquivalent', function (BotMan $bot) {
+            $bot->reply('Please specify the value as well as the currency, eg: /getBTCEquivalent 100 '.BotConfig::get('currency'));
         });
 
         $botman->hears('/getUserID', function (BotMan $bot) {
@@ -89,5 +105,14 @@ class BotmanContoller extends Controller
         $message = "$value {$currentPrice->currency} is ".number_format($valueInBtc, 6)." BTC (".number_format($exchangeRate, 4)." {$currentPrice->currency} - 1 BTC)";
 
         return $message;
+    }
+
+    private function getAvailableCommands()
+    {
+        return "Available commands are:\n\n".
+        '/getBTCEquivalent {value} {currency} # Returns the Bitcoin equivalent for a specific currency.
+         eg: /getBTCEquivalent 100 '.BotConfig::get('currency')."\n".
+        "/getUserID # Returns your registered user ID for the system\n".
+        '/help # Returns this list of possible commands';
     }
 }
